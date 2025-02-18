3f90cba3a442017f5b2fb440c082a2e6b1b90bc3
Alina Shabalina <alinavshabalina@gmail.com>
2025-02-18 23:06:07 +0100

    fix: delete unnecessary data

--- index.js
+++ index.js
@@ -14,37 +14,27 @@ const stainGrowth = [
     {transform: 'scale(75)'},
 ];
 
-const shirtColor = [
-    {transform: 'scale(1)'},
-];
-
 const stainTiming = {
     duration: 5000,
     iterations: 1,
 };
 
-const shirtTiming = {
-    duration: 2000,
-    iterations: 1,
-};
 
 const stain = document.querySelector('.stain')
 const shirt = document.querySelector('.shirt')
 const wrapper = document.querySelector('.wrapper')
-shirt.addEventListener("click",  (e) => {
-        stain.style.top = `${e.clientY - 5}px`
-        stain.style.left = `${e.clientX -5}px`
-        stain.style.visibility = 'visible'
-        wrapper.style.maskImage = "url('public/mask.svg')"
-        wrapper.style.maskPosition = "center"
-        wrapper.style.maskClip = "view-box"
-        wrapper.style.maskPosition = "center"
-        wrapper.style.maskSize = "72%"
-        wrapper.style.maskRepeat = "no-repeat"
-        const animationStain = stain.animate(stainGrowth, stainTiming);
-        animationStain.addEventListener('finish', function() {
+shirt.addEventListener("click", (e) => {
+    stain.style.top = `${e.clientY - 5}px`
+    stain.style.left = `${e.clientX - 5}px`
+    stain.style.visibility = 'visible'
+    wrapper.style.maskImage = "url('public/mask.svg')"
+    wrapper.style.maskPosition = "center"
+    wrapper.style.maskClip = "view-box"
+    wrapper.style.maskPosition = "center"
+    wrapper.style.maskSize = "72%"
+    wrapper.style.maskRepeat = "no-repeat"
+    const animationStain = stain.animate(stainGrowth, stainTiming);
+    animationStain.addEventListener('finish', function () {
         stain.style.transform = 'scale(250)'
     });
-
-
 });
