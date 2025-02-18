4a2fcdd84603ff236e05bf5745c29cff037e8dfd
Alina Shabalina <alinavshabalina@gmail.com>
2025-02-18 22:57:46 +0100

    feat: add a mask to the shirt

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-This project 'Bloody shirt' (pun intended) aims at exploring the depths of animations & bundling.
+This project 'Bloody shirt' (pun intended) aims at exploring the depths of animations, masking & bundling.
 
 It is for now created with vanilla JavaScript, CSS & HTML and further will be changed to TypeScript & SCSS.
 
--- index.js
+++ index.js
@@ -3,6 +3,19 @@ const stainGrowth = [
     {transform: 'scale(1.5)'},
     {transform: 'scale(2)'},
     {transform: 'scale(2.5)'},
+    {transform: 'scale(3)'},
+    {transform: 'scale(5)'},
+    {transform: 'scale(7)'},
+    {transform: 'scale(10)'},
+    {transform: 'scale(15)'},
+    {transform: 'scale(20)'},
+    {transform: 'scale(22)'},
+    {transform: 'scale(50)'},
+    {transform: 'scale(75)'},
+];
+
+const shirtColor = [
+    {transform: 'scale(1)'},
 ];
 
 const stainTiming = {
@@ -10,14 +23,28 @@ const stainTiming = {
     iterations: 1,
 };
 
+const shirtTiming = {
+    duration: 2000,
+    iterations: 1,
+};
+
 const stain = document.querySelector('.stain')
 const shirt = document.querySelector('.shirt')
+const wrapper = document.querySelector('.wrapper')
 shirt.addEventListener("click",  (e) => {
         stain.style.top = `${e.clientY - 5}px`
         stain.style.left = `${e.clientX -5}px`
         stain.style.visibility = 'visible'
-        const animation = stain.animate(stainGrowth, stainTiming);
-        animation.addEventListener('finish', function() {
-        stain.style.transform = 'scale(2.5)';
+        wrapper.style.maskImage = "url('public/mask.svg')"
+        wrapper.style.maskPosition = "center"
+        wrapper.style.maskClip = "view-box"
+        wrapper.style.maskPosition = "center"
+        wrapper.style.maskSize = "72%"
+        wrapper.style.maskRepeat = "no-repeat"
+        const animationStain = stain.animate(stainGrowth, stainTiming);
+        animationStain.addEventListener('finish', function() {
+        stain.style.transform = 'scale(250)'
     });
+
+
 });
