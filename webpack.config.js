5f2cba15bb7408fe95a10937433d6c7d70b5e025
Alina Shabalina <alinavshabalina@gmail.com>
2025-02-20 11:23:57 +0100

    feat: added webpack.config.js still not working

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,26 @@
-const {resolve} = require("node:path");
+const path = require('path')
+
 module.exports = {
-    entry: './index.ts',
+    entry: './src/index.ts',
     module: {
         rules: [
-            { test: /\.svg$/, use: 'svg-inline-loader' },
-            { test: /\.(ts)$/, use: 'babel-loader' }
-        ]
+            {
+                test: /\.ts?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
     },
     output: {
-        path: resolve(__dirname, 'dist'),
-        filename: 'index_bundle.js'
-    }
-}
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    devServer: {
+        static: path.join(__dirname, "dist"),
+        compress: true,
+        port: 4000,
+    },
+};
